39fe7ae5cd95e11f2eb50ff857a20dd4052c8550
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-04-19 23:55:21 -0600

    create form

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -8,18 +8,17 @@ function Page() {
   const handleChange = (e) => {
     console.log(e.target.value);
   };
-    return (
-      <form>
-        <inpt name="title" placeholder="Title" onChange={handleChange} />
-        <textarea
-          name="description"
-          placeholder="Content"
-          onChange={handleChange}
-        />
-        <button type="submit">Save</button>
-      </form>
-    );
-  }
+  return (
+    <form>
+      <inpt name="title" placeholder="Title" onChange={handleChange} />
+      <textarea
+        name="description"
+        placeholder="Content"
+        onChange={handleChange}
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
+}
 
 export default Page;
-
